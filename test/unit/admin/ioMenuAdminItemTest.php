e0379ffa25a95f6c46fd4a59c9f616b61dc01976
Ryan Weaver <ryan@thatsquality.com>
2010-05-30 10:09:38 -0500

    Fixing admin test - bad bootstrap require.

--- test/unit/admin/ioMenuAdminItemTest.php
+++ test/unit/admin/ioMenuAdminItemTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__).'/../bootstrap/functional.php';
+require_once dirname(__FILE__).'/../../bootstrap/functional.php';
 require_once $_SERVER['SYMFONY'].'/vendor/lime/lime.php';
 
 $t = new lime_test(5);
